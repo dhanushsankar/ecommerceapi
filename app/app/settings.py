dea529b493351a86e4631b1cd37c45333b237c50
dhanush sankar <s.dhanush1998@gmail.com>
2023-01-28 18:56:29 +0530

    Added user_api.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework.authtoken',
+    'rest_framework',
+    'drf_spectacular',
+    'user',
+    
 ]
 
 MIDDLEWARE = [
@@ -129,3 +134,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
